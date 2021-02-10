b10c6252b32b06cd60fc4a00da67bfd8d9c2f432
Pedro Miyake <pedromiyake@gmail.com>
2021-02-10 14:52:27 +0100

    Created component for testing CSS loader

---
+++ src/components/hello-world-button/hello-world-button.js
@@ -0,0 +1,22 @@
+import './hello-world-button.css';
+
+class HelloWorldButton {
+  
+  render() {
+    const body = document.querySelector('body');
+    const button = document.createElement('button');
+    button.innerHTML = 'Hello World';
+    button.classList.add('hello-world-button');
+    button.onclick = function() {
+      const p = document.createElement('p');
+      p.innerHTML = 'Hello World';
+      p.classList.add('hello-world-text');
+      body.appendChild(p);
+    }
+    body.appendChild(button);
+    console.log(body);
+  }
+
+}
+
+export default HelloWorldButton;
