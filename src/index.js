2122278a35f07ff5501b3aa3128fffdc6a875bce
Pedro Miyake <pedromiyake@gmail.com>
2021-02-10 14:53:24 +0100

    Removed add image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import helloWorld from './hello-world';
-import addImage from './add-image';
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
 
-helloWorld();
-addImage();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
